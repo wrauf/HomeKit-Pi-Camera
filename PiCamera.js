1ce2824f909222cca23423bac678a4f44e5c6abb
Waqas Rauf <waqas@WR-MBP.local>
2018-02-07 03:01:16 +0000

    updated missing liberaries added the flag for strict mode.

--- PiCamera.js
+++ PiCamera.js
@@ -1,4 +1,7 @@
-/***********************************Camera details***********************************************/
+"use strict";
+
+//Camera details - START
+
 var CameraName = "Pi Camera";
 var CameraManufacturer = "Raspberry Pi";
 var CameraModel = "HD Camera Board V2";
@@ -7,27 +10,25 @@ var CameraFirmwareRevision = "1.0.0";
 
 var CameraUserName = "EC:22:3D:D3:CE:CE";
 var CameraPort = 51062;
-var CameraPinCOde = "031-45-154";
+var CameraPinCode = "031-45-154";
 
 var CameraResolutionWidth = 1920;
 var CameraResolutionHeight = 1080;
 var CameraFPS = 30;
 var CameraBitRate = 300;
 
-/***********************************Camera details***********************************************/
+//Camera details - END
 
-var storage = require('node-persist');
-var uuid = require('hap-nodejs').uuid;
-var Accessory = require('hap-nodejs').Accessory;
-var Camera = require('hap-nodejs').Camera;
+var storage = require("node-persist");
+var uuid = require("hap-nodejs").uuid;
+var Service = require("hap-nodejs").Service;
+var Characteristic = require("hap-nodejs").Characteristic;
+var Accessory = require("hap-nodejs").Accessory;
+var Camera = require("hap-nodejs").Camera;
 
-
-//var core = require('hap-nodejs/CameraCore.js');
-//var Camera = require('hap-nodejs').Camera;
-//var uuid = require('hap-nodejs/lib/util/uuid.js');
-const spawn = require('child_process').spawn;
-var shell = require('shelljs');
-var fs = require('fs');
+const spawn = require("child_process").spawn;
+var shell = require("shelljs");
+var fs = require("fs");
 
 console.log("HAP-NodeJS starting...");
 
@@ -55,12 +56,12 @@ var cameraSource = new Camera();
 
 // Snaphost request handler
 Camera.prototype.handleSnapshotRequest = function (request, callback) {
-  var raspistill = `raspistill -w ${request.width} -h ${request.height} -t 10 -o ./snapshots/snapshot.jpg`;
+  var raspistill = "raspistill -w ${request.width} -h ${request.height} -t 10 -o ./snapshots/snapshot.jpg";
 
   shell.exec(raspistill, function (code, stdout, stderr) {
     var snapshot = undefined;
     if (code === 0) {
-      snapshot = fs.readFileSync(__dirname + '/snapshots/snapshot.jpg');
+      snapshot = fs.readFileSync(__dirname + "/snapshots/snapshot.jpg");
     }
     callback(stderr, snapshot);
   });
@@ -93,18 +94,17 @@ Camera.prototype.handleStreamRequest = function (request) {
           }
 
           bitrate = videoInfo["max_bit_rate"];
-          console.log('bitrate: ', bitrate);
+          console.log('bitrate: , bitrate);
         }
 
         let targetAddress = sessionInfo["address"];
         let targetVideoPort = sessionInfo["video_port"];
         let videoKey = sessionInfo["video_srtp"];
 
-        // let ffmpegCommand = '-f video4linux2 -i /dev/video0 -threads 0 -vcodec libx264 -an -pix_fmt yuv420p -r '+ fps +' -f rawvideo -tune zerolatency -vf scale=w='+ width +':h='+ height +' -b:v '+ bitrate +'k -bufsize '+ bitrate +'k -payload_type 99 -ssrc 1 -f rtp -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params '+videoKey.toString('base64')+' srtp://'+targetAddress+':'+targetVideoPort+'?rtcpport='+targetVideoPort+'&localrtcpport='+targetVideoPort+'&pkt_size=1378';
-        let ffmpegCommand = '-f video4linux2 -i /dev/video0 -s ' + width + ':' + height + ' -threads auto -vcodec h264 -an -pix_fmt yuv420p -f rawvideo -tune zerolatency -vf scale=w=' + width + ':h=' + height + ' -b:v ' + bitrate + 'k -bufsize ' + 2 * bitrate + 'k -payload_type 99 -ssrc 1 -f rtp -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params ' + videoKey.toString('base64') + ' srtp://' + targetAddress + ':' + targetVideoPort + '?rtcpport=' + targetVideoPort + '&localrtcpport=' + targetVideoPort + '&pkt_size=1378';
+        let ffmpegCommand = "-f video4linux2 -i /dev/video0 -s " + width + ":" + height + " -threads auto -vcodec h264 -an -pix_fmt yuv420p -f rawvideo -tune zerolatency -vf scale=w=" + width + ":h=" + height + " -b:v " + bitrate + "k -bufsize " + 2 * bitrate + "k -payload_type 99 -ssrc 1 -f rtp -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params " + videoKey.toString("base64") + " srtp://" + targetAddress + ":" + targetVideoPort + "?rtcpport=" + targetVideoPort + "&localrtcpport=" + targetVideoPort + "&pkt_size=1378";
 
-        console.log('avconv stream: ', ffmpegCommand);
-        let ffmpeg = spawn('avconv', ffmpegCommand.split(' '), { env: process.env });
+        console.log("avconv stream: ", ffmpegCommand);
+        let ffmpeg = spawn("avconv", ffmpegCommand.split(" "), { env: process.env });
         this.ongoingSessions[sessionIdentifier] = ffmpeg;
       }
 
@@ -112,7 +112,7 @@ Camera.prototype.handleStreamRequest = function (request) {
     } else if (requestType == "stop") {
       var ffmpegProcess = this.ongoingSessions[sessionIdentifier];
       if (ffmpegProcess) {
-        ffmpegProcess.kill('SIGKILL');
+        ffmpegProcess.kill("SIGKILL");
       }
 
       delete this.ongoingSessions[sessionIdentifier];
@@ -125,7 +125,7 @@ cameraAccessory.configureCameraSource(cameraSource);
 console.log("Camera source configured......");
 
 //hook up events.
-cameraAccessory.on('identify', function(paired, callback) {
+cameraAccessory.on("identify", function(paired, callback) {
   console.log(CameraName + " identify invoked...");
   callback(); // success
 });
@@ -133,8 +133,8 @@ cameraAccessory.on('identify', function(paired, callback) {
 // Publish the camera on the local network.
 cameraAccessory.publish({
   username: CameraUserName,
-  port: CameraPinCOde,
-  pincode: CameraPinCOde,
+  port: CameraPort,
+  pincode: CameraPinCode,
   category: Accessory.Categories.CAMERA
 }, true);
 
